1e518ace98e86dfe407c02d32fdd4d09291aa624
Vladdddy <vlad00anonimous00@gmail.com>
2025-05-22 11:11:43 +0200

    Installing package classnames

--- src/components/Selector.jsx
+++ src/components/Selector.jsx
@@ -2,22 +2,29 @@ import React from "react";
 import classNames from "classnames";
 import "../styles/selector.css";
 
-const Selector = ({ icon: Icon, text, active, onClick, isLogout = false, className }) => {
-  return (
-    <div
-      onClick={onClick}
-      style={{ cursor: "pointer" }}
-      className={classNames(
-        "selector",
-        { active },
-        { logout: isLogout },
-        className
-      )}
-    >
-      <Icon className="selector-icon" active={active} />
-      <span className="selector-text">{text}</span>
-    </div>
-  );
+const Selector = ({
+    icon: Icon,
+    text,
+    active,
+    onClick,
+    isLogout = false,
+    className,
+}) => {
+    return (
+        <div
+            onClick={onClick}
+            style={{ cursor: "pointer" }}
+            className={classNames(
+                "selector",
+                { active },
+                { logout: isLogout },
+                className
+            )}
+        >
+            <Icon className="selector-icon" active={active} />
+            <span className="selector-text">{text}</span>
+        </div>
+    );
 };
 
 export default Selector;
