2b4b8531787b5ce089bd76086aa94d07ff33fda9
Vladdddy <vlad00anonimous00@gmail.com>
2025-05-23 14:05:45 +0200

    Css changes to the addVisits page

--- src/pages/AddVisit.jsx
+++ src/pages/AddVisit.jsx
@@ -1,54 +1,56 @@
-import React from 'react';
-import '../styles/addvisit.css';
+import React from "react";
+import "../styles/addvisit.css";
 
 const AddVisit = () => {
-  return (
-    <div className="add-visit-container">
-      <h1>Aggiungi visita</h1>
-      <form className="visit-form">
-        <div className="form-row">
-          <div className="input-group">
-            <label>Nome e cognome</label>
-            <input type="text" />
-          </div>
-          <div className="input-group">
-            <label>Data visita</label>
-            <input type="date" />
-          </div>
-        </div>
+    return (
+        <div className="add-visit-container">
+            <h1>Aggiungi visita</h1>
+            <form className="visit-form">
+                <div className="form-row">
+                    <div className="input-group">
+                        <label>Nome e cognome</label>
+                        <input type="text" />
+                    </div>
+                    <div className="input-group">
+                        <label>Data visita</label>
+                        <input type="date" />
+                    </div>
+                </div>
 
-        <div className="form-row">
-          <div className="input-group">
-            <label>Numero di telefono</label>
-            <input type="tel" />
-          </div>
-          <div className="input-group">
-            <label>Orario inizio</label>
-            <input type="time" />
-          </div>
-        </div>
+                <div className="form-row">
+                    <div className="input-group">
+                        <label>Numero di telefono</label>
+                        <input type="tel" />
+                    </div>
+                    <div className="input-group">
+                        <label>Orario inizio</label>
+                        <input type="time" />
+                    </div>
+                </div>
 
-        <div className="form-row">
-          <div className="input-group">
-            <label>Email</label>
-            <input type="email" />
-          </div>
-          <div className="input-group">
-            <label>Orario fine</label>
-            <input type="time" />
-          </div>
-        </div>
+                <div className="form-row">
+                    <div className="input-group">
+                        <label>Email</label>
+                        <input type="email" />
+                    </div>
+                    <div className="input-group">
+                        <label>Orario fine</label>
+                        <input type="time" />
+                    </div>
+                </div>
 
-        <div className="form-row">
-          <div className="input-group">
-            <label>Codice fiscale</label>
-            <input type="text" />
-          </div>
-          <button type="submit" className="prenota-button">Prenota</button>
+                <div className="form-row">
+                    <div className="input-group">
+                        <label>Codice fiscale</label>
+                        <input type="text" />
+                    </div>
+                    <button type="submit" className="prenota-button">
+                        Prenota
+                    </button>
+                </div>
+            </form>
         </div>
-      </form>
-    </div>
-  );
+    );
 };
 
 export default AddVisit;
